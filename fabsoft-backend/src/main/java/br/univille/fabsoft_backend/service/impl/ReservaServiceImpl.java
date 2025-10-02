653b60d65e6040e5f701e6443b7cb91401a48040
João Palhano <167135734+JoaoPalhano07@users.noreply.github.com>
2025-10-02 17:25:05 +0000

    Ajustes

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/service/impl/ReservaServiceImpl.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/service/impl/ReservaServiceImpl.java
@@ -5,7 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.univille.fabsoft_backend.entity.Cliente;
+import br.univille.fabsoft_backend.entity.ModalidadeEsportiva;
+import br.univille.fabsoft_backend.entity.Quadra;
 import br.univille.fabsoft_backend.entity.Reserva;
+import br.univille.fabsoft_backend.repository.ClienteRepository;
+import br.univille.fabsoft_backend.repository.ModalidadeEsportivaRepository;
+import br.univille.fabsoft_backend.repository.QuadraRepository;
 import br.univille.fabsoft_backend.repository.ReservaRepository;
 import br.univille.fabsoft_backend.service.ReservaService;
 
@@ -15,6 +21,15 @@ public class ReservaServiceImpl
 
     @Autowired
     private ReservaRepository repository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private QuadraRepository quadraRepository;
+
+    @Autowired
+    private ModalidadeEsportivaRepository modalidadeEsportivaRepository;
 
     @Override
     public List<Reserva> getAll() {
@@ -39,6 +54,21 @@ public class ReservaServiceImpl
         reservaAntiga.setDataHoraFim(reserva.getDataHoraFim());
         reservaAntiga.setValorTotal(reserva.getValorTotal());
         reservaAntiga.setStatus(reserva.getStatus());
+            if (reserva.getQuadra() != null && reserva.getQuadra().getId() != 0) {
+        Quadra quadra = quadraRepository.findById(reserva.getQuadra().getId())
+            .orElseThrow(() -> new Exception("A Quadra com o ID informado não existe: " + reserva.getQuadra().getId()));
+        reservaAntiga.setQuadra(quadra);
+    }
+    if (reserva.getCliente() != null && reserva.getCliente().getId() != 0) {
+        Cliente cliente = clienteRepository.findById(reserva.getCliente().getId())
+            .orElseThrow(() -> new Exception("O Cliente com o ID informado não existe: " + reserva.getCliente().getId()));
+        reservaAntiga.setCliente(cliente);
+    }
+    if (reserva.getModalidadeEsportiva() != null && reserva.getModalidadeEsportiva().getId() != 0) {
+        ModalidadeEsportiva modalidade = modalidadeEsportivaRepository.findById(reserva.getModalidadeEsportiva().getId())
+            .orElseThrow(() -> new Exception("A Modalidade Esportiva com o ID informado não existe: " + reserva.getModalidadeEsportiva().getId()));
+        reservaAntiga.setModalidadeEsportiva(modalidade);
+    }
         repository.save(reservaAntiga);
         return reservaAntiga;
 
